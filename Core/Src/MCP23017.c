5b147d004ff4c25751ecb272aaa7f59d3fdb041e
hemeng <koloonps@gmail.com>
2025-04-13 22:17:22 +0800

    修复无用代码

--- Core/Src/MCP23017.c
+++ Core/Src/MCP23017.c
@@ -2,3 +2,34 @@
 // Created by hemeng on 2025/4/12.
 //
 #include "MCP23017.h"
+
+
+/**
+ *
+MCP23017_GPIO_t gpio_a;
+    gpio_a.bits.io0 = 0;
+    gpio_a.bits.io1 = 0;
+    gpio_a.bits.io2 = 0;
+    gpio_a.bits.io3 = 0;
+    gpio_a.bits.io4 = 1;
+    gpio_a.bits.io5 = 1;
+    gpio_a.bits.io6 = 1;
+    gpio_a.bits.io7 = 1;
+    MCP23017_GPIO_t gpio_b;
+    gpio_b.bits.io0 = 0;
+    gpio_b.bits.io1 = 0;
+    gpio_b.bits.io2 = 0;
+    gpio_b.bits.io3 = 0;
+    gpio_b.bits.io4 = 0;
+    gpio_b.bits.io5 = 0;
+    gpio_b.bits.io6 = 0;
+    gpio_b.bits.io7 = 0;
+    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_IODIRA, I2C_MEMADD_SIZE_8BIT, &gpio_a.all, 1,
+                      100);
+    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_IODIRB, I2C_MEMADD_SIZE_8BIT, &gpio_b.all, 1,
+                      100);
+    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_OLATA, I2C_MEMADD_SIZE_8BIT, &gpio_a.all, 1,
+                      100);
+    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_OLATB, I2C_MEMADD_SIZE_8BIT, &gpio_b.all, 1,
+                      100);
+ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -68,9 +68,7 @@ float Temperature, Pressure, Humidity;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-
 static void MPU_Config(void);
-
 /* USER CODE BEGIN PFP */
 
 
@@ -87,7 +85,7 @@ uint32_t packet_seq = 0;
 uint8_t rxIndex = 0;
 char rxBuffer[BUFFER_SIZE];
 uint8_t uartReceiveByte;
-uint8_t mark = 0;
+
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
     if (GPIO_Pin == GPIO_PIN_10) {
@@ -97,18 +95,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
         DS3231_ReadStatusReg(&ds3231_status_reg);
         ds3231_status_reg.bits.A1F = 0;
         DS3231_WriteStatusReg(&ds3231_status_reg);
-        MCP23017_GPIO_t gpio_b;
-        mark = mark == 1 ? 0 : 1;
-        gpio_b.bits.io0 = mark;
-        gpio_b.bits.io1 = mark;
-        gpio_b.bits.io2 = mark;
-        gpio_b.bits.io3 = mark;
-        gpio_b.bits.io4 = mark;
-        gpio_b.bits.io5 = mark;
-        gpio_b.bits.io6 = mark;
-        gpio_b.bits.io7 = mark;
-        HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_OLATB, I2C_MEMADD_SIZE_8BIT, &gpio_b.all, 1,
-                          100);
     }
 }
 
@@ -305,40 +291,42 @@ void set_system_time(const time_t seconds, uint32_t us) {
   * @brief  The application entry point.
   * @retval int
   */
-int main(void) {
-    /* USER CODE BEGIN 1 */
+int main(void)
+{
 
-    /* USER CODE END 1 */
+  /* USER CODE BEGIN 1 */
 
-    /* MPU Configuration--------------------------------------------------------*/
-    MPU_Config();
+  /* USER CODE END 1 */
 
-    /* MCU Configuration--------------------------------------------------------*/
+  /* MPU Configuration--------------------------------------------------------*/
+  MPU_Config();
 
-    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-    HAL_Init();
+  /* MCU Configuration--------------------------------------------------------*/
 
-    /* USER CODE BEGIN Init */
-    /* USER CODE END Init */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
 
-    /* Configure the system clock */
-    SystemClock_Config();
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
 
-    /* USER CODE BEGIN SysInit */
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
 
 
-    /* USER CODE END SysInit */
+  /* USER CODE END SysInit */
 
-    /* Initialize all configured peripherals */
-    MX_GPIO_Init();
-    MX_DMA_Init();
-    MX_USART1_UART_Init();
-    MX_I2C1_Init();
-    MX_LWIP_Init();
-    MX_IWDG1_Init();
-    MX_CRC_Init();
-    MX_I2C3_Init();
-    /* USER CODE BEGIN 2 */
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART1_UART_Init();
+  MX_I2C1_Init();
+  MX_LWIP_Init();
+  MX_IWDG1_Init();
+  MX_CRC_Init();
+  MX_I2C3_Init();
+  /* USER CODE BEGIN 2 */
     HAL_UART_Receive_IT(&huart1, &uartReceiveByte, 1);
     if (HAL_OK != EEPROM_Init(&hi2c1)) {
         printf("初始化eeprom失败\n");
@@ -348,7 +336,6 @@ int main(void) {
     if (HAL_OK == DS3231_ReadControlReg(&ds3231_control_reg)) {
         DS3231_SetAlarm1(0, 0, 0, 1, 0x0E);
     }
-    DS3231_ReadSQWConfig();
     DS3231_TimeType rtcTime;
     DS3231_GetTime(&rtcTime);
     printf("启动时间:%04d-%02d-%02d %02d:%02d:%02d\r\n",
@@ -396,43 +383,16 @@ int main(void) {
             NVIC_SystemReset();
         }
     }
-    MCP23017_GPIO_t gpio_a;
-    gpio_a.bits.io0 = 0;
-    gpio_a.bits.io1 = 0;
-    gpio_a.bits.io2 = 0;
-    gpio_a.bits.io3 = 0;
-    gpio_a.bits.io4 = 1;
-    gpio_a.bits.io5 = 1;
-    gpio_a.bits.io6 = 1;
-    gpio_a.bits.io7 = 1;
-    MCP23017_GPIO_t gpio_b;
-    gpio_b.bits.io0 = 0;
-    gpio_b.bits.io1 = 0;
-    gpio_b.bits.io2 = 0;
-    gpio_b.bits.io3 = 0;
-    gpio_b.bits.io4 = 0;
-    gpio_b.bits.io5 = 0;
-    gpio_b.bits.io6 = 0;
-    gpio_b.bits.io7 = 0;
-    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_IODIRA, I2C_MEMADD_SIZE_8BIT, &gpio_a.all, 1,
-                      100);
-    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_IODIRB, I2C_MEMADD_SIZE_8BIT, &gpio_b.all, 1,
-                      100);
-    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_OLATA, I2C_MEMADD_SIZE_8BIT, &gpio_a.all, 1,
-                      100);
-    HAL_I2C_Mem_Write(&hi2c3, MCP23017_ADDRESS, MCP23017_OLATB, I2C_MEMADD_SIZE_8BIT, &gpio_b.all, 1,
-                      100);
+  /* USER CODE END 2 */
 
-    /* USER CODE END 2 */
-
-    /* Infinite loop */
-    /* USER CODE BEGIN WHILE */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
     while (1) {
-        /* USER CODE END WHILE */
+    /* USER CODE END WHILE */
 
-        /* USER CODE BEGIN 3 */
+    /* USER CODE BEGIN 3 */
         MX_LWIP_Process();
-        for (int i = 0; i < 499999; ++i) {
+        for (int i = 0; i < 1; ++i) {
             MX_LWIP_Process();
         }
         float temp, humi;
@@ -468,112 +428,115 @@ int main(void) {
         }
         cJSON_Delete(packet);
         free(json_str);
-        HAL_I2C_Mem_Read(&hi2c3, MCP23017_ADDRESS, MCP23017_GPIOA, I2C_MEMADD_SIZE_8BIT, &gpio_a.all,
-                         1, 100);
     }
-    /* USER CODE END 3 */
+  /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void) {
-    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** Supply configuration update enable
-    */
-    HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+  /** Supply configuration update enable
+  */
+  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
 
-    /** Configure the main internal regulator output voltage
-    */
-    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
-    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
-    }
+  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
 
-    /** Initializes the RCC Oscillators according to the specified parameters
-    * in the RCC_OscInitTypeDef structure.
-    */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI;
-    RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
-    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-    RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM = 4;
-    RCC_OscInitStruct.PLL.PLLN = 60;
-    RCC_OscInitStruct.PLL.PLLP = 2;
-    RCC_OscInitStruct.PLL.PLLQ = 2;
-    RCC_OscInitStruct.PLL.PLLR = 2;
-    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
-    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-    RCC_OscInitStruct.PLL.PLLFRACN = 0;
-    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-        Error_Handler();
-    }
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_DIV1;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 60;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_3;
+  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
 
-    /** Initializes the CPU, AHB and APB buses clocks
-    */
-    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
-                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
-                                  | RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
-    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
-    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
-        Error_Handler();
-    }
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
 
-/* MPU Configuration */
+ /* MPU Configuration */
 
-void MPU_Config(void) {
-    MPU_Region_InitTypeDef MPU_InitStruct = {0};
+void MPU_Config(void)
+{
+  MPU_Region_InitTypeDef MPU_InitStruct = {0};
 
-    /* Disables the MPU */
-    HAL_MPU_Disable();
+  /* Disables the MPU */
+  HAL_MPU_Disable();
 
-    /** Initializes and configures the Region and the memory to be protected
-    */
-    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-    MPU_InitStruct.BaseAddress = 0x0;
-    MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
-    MPU_InitStruct.SubRegionDisable = 0x87;
-    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-    MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
-    MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  /** Initializes and configures the Region and the memory to be protected
+  */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.BaseAddress = 0x0;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_4GB;
+  MPU_InitStruct.SubRegionDisable = 0x87;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
+  MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
 
-    HAL_MPU_ConfigRegion(&MPU_InitStruct);
-    /* Enables the MPU */
-    HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+  /* Enables the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+
 }
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void) {
-    /* USER CODE BEGIN Error_Handler_Debug */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1) {
     }
-    /* USER CODE END Error_Handler_Debug */
+  /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
