26839d843de9930adc4896530f92ec8f99f019ef
hemeng <koloonps@gmail.com>
2025-04-30 09:24:01 +0800

    修复格式

--- Core/Src/tcp_client_services.c
+++ Core/Src/tcp_client_services.c
@@ -8,7 +8,7 @@
 #include "memory_sections.h"
 
 
-uint32_t lanRxIndex;
+uint32_t lanRxIndex = 0;
 
 extern CRC_HandleTypeDef hcrc;
 
@@ -23,14 +23,14 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
         tcp_close(tpcb);
         NVIC_SystemReset();
     }
-    struct pbuf *q = p;
+    const struct pbuf *q = p;
     while (q != NULL) {
         if (lanRxIndex + q->len > LAN_PACKET_RX_BUFFER_SIZE) {
             printf("超过缓冲区容量\n");
             pbuf_free(p);
             NVIC_SystemReset();
         }
-        uint8_t *data = q->payload;
+        const uint8_t *data = q->payload;
         for (int i = 0; i < q->len; ++i) {
             packet_buffer[lanRxIndex + i] = data[i];
         }
@@ -39,44 +39,6 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
     }
     tcp_recved(tpcb, p->tot_len);
     pbuf_free(p);
-    if (lanRxIndex >= sizeof(struct nshead_t)) {
-        struct nshead_t nshead = bytes_to_head(packet_buffer);
-        if (NSHEAD_MAGICNUM != nshead.magic_num) {
-            printf("magic错误\n");
-            NVIC_SystemReset();
-        }
-        const uint32_t body_len = nshead.body_len;
-        const uint32_t msg_len = sizeof(struct nshead_t) + body_len;
-        if (msg_len > lanRxIndex) {
-            return ERR_OK;
-        }
-        lanRxIndex -= msg_len;
-        if (body_len > 0) {
-            uint8_t *src = packet_buffer + sizeof(struct nshead_t);
-            const uint32_t actual_crc = HAL_CRC_Calculate(&hcrc, (uint32_t *) src, body_len);
-            if (actual_crc != nshead.checksum) {
-                printf("crc error  actual_crc:%x checksum:%x body_len:%d\n", actual_crc, nshead.checksum, body_len);
-                NVIC_SystemReset();
-            }
-            if (body_len > 1024) {
-                printf("消息解析结束 len:%d timestamp:%d cmd:%04X checksum:%04X  \n", msg_len, nshead.timestamp, nshead.cmd,
-                       nshead.checksum);
-            } else {
-                printf("消息解析结束 len:%d timestamp:%d cmd:%04X checksum:%04X body:%.*s\n", msg_len, nshead.timestamp,
-                       nshead.cmd,
-                       nshead.checksum, body_len, (char *) src);
-            }
-        }
-        if (nshead.cmd == PONG) {
-        } else if (nshead.cmd == DOWNLOAD_BIG_DATA) {
-            for (int i = 0; i < 16; ++i) {
-                ack_message_id[i] = nshead.message_id[i];
-            }
-            ack_flag = 1;
-        }
-        memmove(&packet_buffer[0], &packet_buffer[msg_len], lanRxIndex * sizeof(packet_buffer[0]));
-        memset(&packet_buffer[lanRxIndex], 0, (LAN_PACKET_RX_BUFFER_SIZE - lanRxIndex) * sizeof(packet_buffer[0]));
-    }
     return ERR_OK;
 }
 
@@ -144,6 +106,44 @@ void print_message_id(uint8_t message_id[16]) {
 
 void process_data() {
     HAL_IWDG_Refresh(&hiwdg1);
+    if (lanRxIndex >= sizeof(struct nshead_t)) {
+        struct nshead_t nshead = bytes_to_head(packet_buffer);
+        if (NSHEAD_MAGICNUM != nshead.magic_num) {
+            printf("magic错误\n");
+            NVIC_SystemReset();
+        }
+        const uint32_t body_len = nshead.body_len;
+        const uint32_t msg_len = sizeof(struct nshead_t) + body_len;
+        if (msg_len <= lanRxIndex) {
+            if (body_len > 0) {
+                uint8_t *src = packet_buffer + sizeof(struct nshead_t);
+                const uint32_t actual_crc = HAL_CRC_Calculate(&hcrc, (uint32_t *) src, body_len);
+                if (actual_crc != nshead.checksum) {
+                    printf("crc error  actual_crc:%x checksum:%x body_len:%d\n", actual_crc, nshead.checksum, body_len);
+                    NVIC_SystemReset();
+                }
+                if (body_len > 1024) {
+                    printf("消息解析结束 len:%d timestamp:%d cmd:%04X checksum:%04X  \n", msg_len, nshead.timestamp,
+                           nshead.cmd,
+                           nshead.checksum);
+                } else {
+                    printf("消息解析结束 len:%d timestamp:%d cmd:%04X checksum:%04X body:%.*s\n", msg_len, nshead.timestamp,
+                           nshead.cmd,
+                           nshead.checksum, body_len, (char *) src);
+                }
+            }
+            if (nshead.cmd == PONG) {
+            } else if (nshead.cmd == DOWNLOAD_BIG_DATA) {
+                for (int i = 0; i < 16; ++i) {
+                    ack_message_id[i] = nshead.message_id[i];
+                }
+                ack_flag = 1;
+            }
+            lanRxIndex -= msg_len;
+            memmove(&packet_buffer[0], &packet_buffer[msg_len], lanRxIndex * sizeof(packet_buffer[0]));
+            memset(&packet_buffer[lanRxIndex], 0, (LAN_PACKET_RX_BUFFER_SIZE - lanRxIndex) * sizeof(packet_buffer[0]));
+        }
+    }
     if (tcp_connected_flag && heartbeat_flag) {
         heartbeat_flag = 0;
         heartbeat_handler();
